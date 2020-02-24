704726367252adf43a0ae193b9c906546bd79da5
wanax1 <gary.wan@outlook.com>
2020-02-24 23:07:50 +0800

    git orign test

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+// Copyright (c) 2017, 2019, Oracle and/or its affiliates. All rights reserved.
+# git test
+variable "tenancy_ocid" {} 
+variable "compartment_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "ssh_public_key" {}
+variable "ssh_private_key" {}
+variable "region" {}
